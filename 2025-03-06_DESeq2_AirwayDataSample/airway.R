5e7707d2c4c0011c1387cc20aab437e0b14f45cd
stojapayne <stoja@uthscsa.edu>
2025-03-06 14:22:47 -0600

    Updated r file to fill out sample code
    DESeq2 analysis of airway sample

--- 2025-03-06_DESeq2_AirwayDataSample/airway.R
+++ 2025-03-06_DESeq2_AirwayDataSample/airway.R
@@ -28,34 +28,55 @@ dds <- DESeqDataSet(se, design = ~ cell + dex)
 ## plot heatmap of Poisson distances between samples
 # don't worry about the rownames! You can leave that line out.
 
-
+poisd2 <- PoissonDistance(t(counts(dds)))
+airwayPoisDistMatrix <- as.matrix( poisd2$dd)
+#rownames(samplePoisDistMatrix) <- ddsHTSeq$condition
+colnames(airwayPoisDistMatrix) <- NULL
+#colors = colorRampPalette( rev(brewer.pal(9, "Blues")) )(255) ;  for some reason this didn't work for me
+pheatmap(airwayPoisDistMatrix,
+         clustering_distance_rows = poisd2$dd,
+         clustering_distance_cols = poisd2$dd,
+         #col = colors
+)
 
 
 ## Perform DESeq2 analysis  
 
+ddsAirway <- DESeq(dds)
 
 
 ## Obtain the results and store in an object called 'results'
 
-
+Airwayresults <- results(ddsAirway)
+Airwayresults
+write.csv(as.data.frame(Airwayresults), file="Airwayresults.csv")
 
 ## plot average expression versus log2 fold change - points are colored red if Padj < 0.1
 
+plotMA(Airwayresults, ylim = c(-4, 4), alpha = 0.1, colSig = "red" )
 
 
 ## add gene annotation to results table
-anno <- AnnotationDbi::select(org.Hs.eg.db, rownames(results), 
+Airwayanno <- AnnotationDbi::select(org.Hs.eg.db, rownames(Airwayresults), 
                               columns=c("ENSEMBL", "ENTREZID", "SYMBOL", "GENENAME"), 
                               keytype="ENSEMBL")
-results = cbind(ENSEMBL = rownames(results), results)
-anno_results <- left_join(as.data.frame(results), anno )
-head(anno_results) 
+Airwayresults = cbind(ENSEMBL = rownames(Airwayresults), Airwayresults)
+Airwayanno_results <- left_join(as.data.frame(Airwayresults), Airwayanno )
+head(Airwayanno_results) 
 
 
 
 ## Plot your favorite volcano plot and send it to Dr. Bunnik
 
-
+EnhancedVolcano(Airwayanno_results, 
+                lab = c(Airwayanno_results$SYMBOL), 
+                x = "log2FoldChange", 
+                y="pvalue", 
+                border = "full", 
+                borderWidth = 1.5, 
+                borderColour = "black", 
+                gridlines.major = FALSE, 
+                gridlines.minor = FALSE)
 
 
     
